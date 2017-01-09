8967dc57e73a13cdaa14ddbc5e9b090b97c7f551
Gustavo Figueira Olegário <gustavo.olegario96@gmail.com>
2017-01-09 10:16:36 -0200

    Still documenting

--- src/shift_left_logical.vhd
+++ src/shift_left_logical.vhd
@@ -1,41 +1,47 @@
 ----------------------------------------------------------------------------------
 -- Company:   Federal University of Santa Catarina
 -- Engineer:  Gustavo Figuera Olegario
--- 
+--
 -- Create Date:    07/27/2016
--- Design Name: 
--- Module Name:  Mux2x1  
+-- Design Name:
+-- Module Name:  Shift Left Logical
 -- Project Name: MIPS MultiCycle
--- Target Devices: 
--- Tool versions: 
--- Description: 
+-- Target Devices:
+-- Tool versions:
+-- Description:
 --
--- Dependencies: 
+-- Dependencies:
 --
--- Revision: 
+-- Revision:
 -- Revision 0.01 - File Created
--- Additional Comments: 
+-- Additional Comments:
 --
 ----------------------------------------------------------------------------------
+--! Use standard library
 library IEEE;
+--! Use logic elements
 use IEEE.std_logic_1164.all;
+
+--! Shift left logical description: a simple shifleft logcial implementation,
+--! using the reserved word 'sll'. The operation of shift left logical is
+--! the same as multplying the number by 2. (Example: 0010 -> 0100)
 
 entity shift_left_logical is
 	generic(
 		width: natural := 8;
-		shift_amount: natural := 2
+		shift_amount: natural := 2 --! Amount of digits that the number will be shifted
 	);
-	
+
 	port(
-		input:  in std_logic_vector(width-1 downto 0);
-		output: out std_logic_vector(width-1 downto 0)
+		input:  in std_logic_vector(width-1 downto 0); --! Number that will be shifted
+		output: out std_logic_vector(width-1 downto 0) --! Result from operation
 	);
 
 end entity;
 
-
+--! @brief Using the reserved word 'sll' we shift the operand.
 architecture shift_left_arch of shift_left_logical is
 begin
 	output <= to_stdLogicVector(to_bitvector(input) sll shift_amount);
-	
+
 end architecture;
--- src/signal_extend.vhd
+++ src/signal_extend.vhd
@@ -1,38 +1,45 @@
 ----------------------------------------------------------------------------------
 -- Company:   Federal University of Santa Catarina
 -- Engineer:  Gustavo Figuera Olegario
--- 
+--
 -- Create Date:    07/27/2016
--- Design Name: 
+-- Design Name:
 -- Module Name:  Signal Extend
 -- Project Name: MIPS MultiCycle
--- Target Devices: 
--- Tool versions: 
--- Description: 
+-- Target Devices:
+-- Tool versions:
+-- Description:
 --
--- Dependencies: 
+-- Dependencies:
 --
--- Revision: 
+-- Revision:
 -- Revision 0.01 - File Created
--- Additional Comments: 
+-- Additional Comments:
 --
 ----------------------------------------------------------------------------------
+--! Use standard library
 library IEEE;
+--! Use logic elements
 use IEEE.std_logic_1164.all;
+
+--! Signal extend description: using this component, we can extend the signal
+--! using as much digits as we want.
 
 entity signal_extend is
 	generic(
 		width_input:  natural := 8;
 		width_output: natural := 16
 	);
-	
+
 	port(
-		input:  in  std_logic_vector(width_input-1 downto 0);
-		output: out std_logic_vector(width_output-1 downto 0)
+		input:  in  std_logic_vector(width_input-1 downto 0); --! Number that will be his signal extended.
+		output: out std_logic_vector(width_output-1 downto 0) --! Result
 	);
-	
+
 end entity;
 
+--! @brief We take the value of signal using a signal to save the bit indication.
+--! Next we just extend the signal using the reserved word 'others'.
 architecture signal_extend_arch of signal_extend is
 	signal aux: std_logic_vector (width_output-1 downto 0);
 begin
@@ -40,5 +47,5 @@ begin
 	aux(width_output-1 downto width_input) <= (others => input(width_input-1));
 
 	output <= aux;
-	
+
 end architecture;
